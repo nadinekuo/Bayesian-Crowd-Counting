3740174fdd42db8d60693fd933d3a37a0df52042
Nadine Kuo <h.n.kuo@student.tudelft.nl>
2024-05-30 14:04:00 +0200

    Also move ann files

--- CVPR2023/train_test_val_split.py
+++ CVPR2023/train_test_val_split.py
@@ -18,16 +18,21 @@ def read_filenames(file_path):
     return filenames
 
 # Function to copy files to the target directory
-def copy_files(filenames, src_dir, dest_dir):
+def copy_files(filenames, img_src_dir, ann_src_dir, dest_dir):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     for filename in filenames:
-        src_path = os.path.join(src_dir, filename)
+        img_src_path = os.path.join(img_src_dir, filename)
+        ann_src_path = os.path.join(ann_src_dir, filename.replace('.jpg', '.xml'))
         dest_path = os.path.join(dest_dir, filename)
-        if os.path.exists(src_path):
-            shutil.copy(src_path, dest_path)
+        if os.path.exists(img_src_path):
+            shutil.copy(img_src_path, dest_path)
         else:
-            print(f"Warning: {src_path} does not exist and will be skipped.")
+            print(f"Warning: {img_src_path} does not exist and will be skipped.")
+        if os.path.exists(ann_src_path):
+            shutil.copy(ann_src_path, dest_path)
+        else:
+            print(f"Warning: {ann_src_path} does not exist and will be skipped.")
 
 # Read the train and test filenames
 train_filenames = read_filenames(train_txt_path)
@@ -35,11 +40,11 @@ test_filenames = read_filenames(test_txt_path)
 val_filenames = read_filenames(val_txt_path)
 
 # Copy the train images
-copy_files(train_filenames, images_dir, train_output_dir)
+copy_files(train_filenames, images_dir, annotations_dir, train_output_dir)
 
 # Copy the test images
-copy_files(test_filenames, images_dir, test_output_dir)
+copy_files(test_filenames, images_dir, annotations_dir, test_output_dir)
 
-copy_files(val_filenames, images_dir, val_output_dir)
+copy_files(val_filenames, images_dir, annotations_dir, val_output_dir)
 
 print("Files have been successfully copied to the Train and Test directories.")
